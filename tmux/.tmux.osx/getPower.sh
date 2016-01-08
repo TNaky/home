b5659e776835146374cc37386a7273d22b0e7331
TNak@MacBookAir <michibata.dev@gmail.com>
2016-01-08 14:54:27 +0900

    電池残量を取得するスクリプトを修正

--- tmux/.tmux.osx/getPower.sh
+++ tmux/.tmux.osx/getPower.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+# 電池残量を取得
 POWER=`/usr/bin/pmset -g ps | tail -n 1 | awk '{print $2}' | sed 's/;//' | sed 's/%//';`
+# 電池残量によって色を設定
+COLOUR=""
 if [ $POWER -gt 60 ]; then
-  echo "⚡ #[fg=colour46]${POWER}%"
+  # 緑
+  COLOUR="#[fg=colour46]"
 elif [ $POWER -gt 30 ]; then
-  echo "⚡ #[fg=colour226]${POWER}%"
+  # 黄
+  COLOUR="#[fg=colour226]"
 else
-  echo "⚡ #[fg=colour1]${POWER}%"
+  # 赤
+  COLOUR="#[fg=colour1]"
 fi
+echo "⚡ ${COLOUR}${POWER}%"
