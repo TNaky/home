8a9e71a3d5bb7983f3edee658353a916156dc85d
TNak@MacBookAir <michibata.dev@gmail.com>
2016-01-08 15:46:52 +0900

    SSID表示色を変更

--- tmux/.tmux.osx/getSSID.sh
+++ tmux/.tmux.osx/getSSID.sh
@@ -1,3 +1,4 @@
 SSID=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I`
 SSID=`echo "${SSID}" | grep -w SSID | awk '{print $2}'`
-echo "${SSID}"
+COLOUR="#[fg=colour246]"
+echo "${COLOUR}${SSID}"
