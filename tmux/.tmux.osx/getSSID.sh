19f1ebb22a950379b95a8e2bf83995a03a1988bd
TNak@MacBookAir <michibata.dev@gmail.com>
2016-01-08 17:45:20 +0900

    Wi-Fi off の際の処理を追加

--- tmux/.tmux.osx/getSSID.sh
+++ tmux/.tmux.osx/getSSID.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
 SSID=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I`
 SSID=`echo "${SSID}" | grep -w SSID | awk '{print $2}'`
-COLOUR="#[fg=colour246]"
-echo "${COLOUR}${SSID}"
+if [ ${SSID} = '' ]; then
+  echo 'Wi-Fi off'
+else
+  COLOUR="#[fg=colour246]"
+  echo "${COLOUR}${SSID}"
+fi
