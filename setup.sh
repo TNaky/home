b3fe9ee32222880619058627a05baaf82829b70d
TNak@MacBookAir <michibata.dev@gmail.com>
2016-01-09 07:06:16 +0900

    Mac初回設定時に必要なインストール用スクリプトを記述

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# OS別の処理
+os=''
+if [[ "$(uname)" == 'Darwin' ]]; then
+  os='mac'
+elif [[ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]]; then
+  os='linux'
+elif [[ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]]; then
+  os='cygwin'
+fi
+
+case "${os}" in
+  'mac' )
+    if ! type brew > /dev/null 2>&1; then
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    export PATH=$PATH:/usr/local/bin/
+    if ! type brew > /dev/null 2>&1; then
+      brew install lua
+      brew install vim --with-lua
+      brew tap sanemat/font
+      brew install --powerline --vim-powerline ricty
+      cp -f /usr/local/Cellar/ricty/*/share/fonts/Ricty*.ttf ~/Library/Fonts/
+      brew install tmux
+      brew install macvim
+      brew install htop-osx
+      brew install nmap
+      brew tig
+      brew install wget
+      brew install source-highlight
+    fi
+    ;;
+esac
