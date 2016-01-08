39e69a655c4688659e9b29776cb0e13fbaf53ceb
TNak@MacBookAir <michibata.dev@gmail.com>
2016-01-08 17:52:16 +0900

    取得情報を別変数に格納する形に変更

--- tmux/.tmux.osx/getSSID.sh
+++ tmux/.tmux.osx/getSSID.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-SSID=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I`
-SSID=`echo "${SSID}" | grep -w SSID | awk '{print $2}'`
+INFO=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I`
+SSID=`echo "${INFO}" | grep -w SSID | awk '{print $2}'`
 if [ ${SSID} = '' ]; then
   echo 'Wi-Fi off'
 else
